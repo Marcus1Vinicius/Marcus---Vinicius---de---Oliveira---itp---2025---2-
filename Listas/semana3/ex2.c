275780be10532f49da7d1518e36511283c76bb68
Marcus <omarcusvinicius96@gmail.com>
2025-09-12 08:37:45 -0300

    questão 2 feita

--- Listas/semana3/ex2.c
+++ Listas/semana3/ex2.c
@@ -6,3 +6,33 @@
 // Além disso, há uma taxa fixa de R$ 15,00 para todos os tipos. Crie um programa que leia o consumo em
 // kWh e o tipo de consumidor (R para residencial, C para comercial, I para industrial) e calcule o valor total da
 // conta.
+
+#include <stdio.h>
+
+int main() {
+    
+    float consumo_kwh;
+    char tipo_residencia;
+
+    printf("Digite o seu consumo por kwh: ");
+    scanf("%f", &consumo_kwh);
+
+    printf("Digite o tipo da sua residência\nR para residencial\nC para comercial\nI para industrial ");
+    scanf(" %c", &tipo_residencia);
+
+    if(tipo_residencia == 'R'){
+        float formula = (0.60 * consumo_kwh) + 15;
+        printf("Você deve pagar: %.2f", formula);
+    }
+    else if(tipo_residencia == 'C'){
+        float formula = (0.48 * consumo_kwh) + 15;
+        printf("Você deve pagar: %.2f", formula);
+    }
+    else if(tipo_residencia == 'I'){
+        float formula = (1.29 * consumo_kwh) + 15;
+        printf("Você deve pagar: %.2f", formula);
+    }
+
+    return 0;
+    
+}
