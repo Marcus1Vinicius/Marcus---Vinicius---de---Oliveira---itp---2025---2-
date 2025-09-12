4561dcb13939ac5fd626cb8ba673d74c50cf0f8c
Marcus <omarcusvinicius96@gmail.com>
2025-09-12 08:24:01 -0300

    primeira questão feita

--- Listas/semana3/ex1.c
+++ Listas/semana3/ex1.c
@@ -5,3 +5,35 @@
 // Entre 18.5 e 24.9: "Peso normal"
 // Entre 25.0 e 29.9: "Sobrepeso"
 // 30.0 ou mais: "Obesidade"
+
+#include <stdio.h>
+
+int main() {
+    
+    float peso, altura, IMC;
+
+    printf("Digite o seu peso: ");
+    scanf("%f", &peso);
+
+    printf("Digite a sua altura: ");
+    scanf("%f", &altura);
+
+    IMC = peso / (altura * altura);
+
+    if(IMC < 18.5){
+        printf("Abaixo do peso, seu IMC é: %.2f", IMC);
+    }
+    else if(IMC <= 24.9){
+        printf("Peso normal, seu IMC é: %.2f", IMC);
+    }
+    else if(IMC <=29.9){
+        printf("Sobrepeso, seu IMC é: %.2f", IMC);
+    }
+    else{
+        printf("Obesidade, seu IMC é: %.2f", IMC);
+    }
+
+
+    return 0;
+    
+}
